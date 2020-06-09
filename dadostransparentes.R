f74a7074efb5d16d47b5b642b7aa3ddba37c6673
jclmntn <jclmntn@gmail.com>
2020-06-09 17:19:50 -0300

    Mudanças na primeira versão do scraper

--- dadostransparentes.R
+++ dadostransparentes.R
@@ -1,44 +1,158 @@
 # Pegando dados do site Dados Transparentes
-
-# Para manipular os dados, usar pipes e funcao map
-library(tidyverse)
-
-# Para ler o HTML do site
+# magrittr para pipes, purrr para map, rvest para scraping, 
+# readr para ler e salvar arquivos em csv, stringr para manipular strings
+# lubridate para manipular datas
+library(readr)
+library(magrittr)
 library(rvest)
+library(purrr)
+library(stringr)
+library(lubridate)
+library(tibble)
 
-# Definindo url e nomes
-url_casos <- 'http://www.dadostransparentes.com.br/tabela1.php'
-url_obitos <- 'http://www.dadostransparentes.com.br/tabela2.php'
-nomes <- c('casos', 'obitos')
+# Essa Função troca o separador decimal
 
-# colococando as url em listas para utilizar na fun??o map
-links <- list(url_casos, url_obitos)
-
-# aqui eu uso fun??es do rvest e tibble para ler o conteudo do site
-# o xpath foi obtido inspecionando os elementos do site
-
-get_web_data <- function(x){
-  read_html(x) %>% 
-    html_nodes(xpath = "//*[@id=\"table\"]") %>% 
-    html_table(header = NA, dec = ",") %>% 
-    .[[1]] %>% 
-    map(., function(x) (str_replace(str_replace(x, "\\.", ""), "\\,", "\\."))) %>% 
-    as_tibble()
+change_separator <- function(x){
+  map(x, function(x) (str_replace(str_replace(x, "\\.", ""), "\\,", "\\.")))
 }
 
+# Essa função coleta a data da última atualização
+get_update <- function(){
+  url <- "https://www.dadostransparentes.com.br/"
+  read_html(url) %>% 
+    html_nodes('iframe') %>% 
+    extract(4) %>% 
+    html_attr("src") %>% 
+    paste0(url, .) %>% 
+    read_html() %>%
+    html_table() %>% 
+    extract2(1) %>% 
+    str_extract(., pattern = '\\d*\\/\\d*\\/\\d*') %>%
+    dmy()
+}  
 
-# usando fun??es do purr para criar os dois objetos e salv?-los em csvs
 
-dados <- map(links, get_web_data)
+get_casos <- function(dec = ".", save = TRUE){
+  
+  url <- "https://www.dadostransparentes.com.br/"
+  
+  casos <- read_html(url) %>% 
+    html_nodes('iframe') %>% 
+    extract(9) %>%
+    html_attr("src") %>% 
+    paste0(url, .) %>% 
+    read_html %>%
+    html_nodes(xpath = "//*[@id=\"table\"]") %>% 
+    html_table(header = NA, dec = ".")%>% 
+    .[[1]]
+  
+  if(dec =="."){
+    casos <- change_separator(casos)
+    if(save == TRUE){
+      is_empty <- ifelse(length(str_detect(list.files("dados"), 'casos')) == 0,
+                         yes = 0, no = str_detect(list.files("dados"), 'casos'))
+      date <- ifelse(is_empty, 
+                     str_extract(list.file(dados), '\\d*\\-\\d*\\-\\d*'),
+                     0)
+      last_file <- ifelse(test = (date > 0), 
+                          yes = get_update() - max(ymd(date)), 
+                          no = get_update())
+      if(last_file > 0){
+        name <- paste0('dados/casos', get_update(), '.csv')
+        
+        casos %>% 
+          as_tibble() %>%
+          write_csv(., name)
+        
+        read_csv(name)
+      }else{
+        as_tibble(casos)
+      }
+    }else{
+      as_tibble(casos)
+    }
+  }else{
+    if(save == TRUE){
+      is_empty <- ifelse(length(str_detect(list.files("dados"), 'casos')) == 0,
+                         yes = 0, no = str_detect(list.files("dados"), 'casos'))
+      date <- ifelse(is_empty, 
+                     str_extract(list.file(dados), '\\d*\\-\\d*\\-\\d*'),
+                     0)
+      last_file <- ifelse(test = (date > 0), 
+                          yes = get_update() - max(ymd(date)), 
+                          no = get_update())
+      if(last_file > 0){
+        casos %>% 
+          as_tibble() %>%
+          write_csv(., paste0('casos', get_update(), '.csv'))
+        
+      }else{
+        as_tibble(casos)
+      }
+    }else{
+      as_tibble(casos)
+    }
+  }
+}
 
-# salvando csvs
-
-map2(
-  dados, 
-  nomes, 
-  function(x, y) write_csv(x, paste0("dados/", y, lubridate::today(), '.csv' ))
-  )
-
-# teste
-
-# read_csv(paste0('casos', lubridate::today(), '.csv'))
+get_obitos <- function(dec = ".", save = TRUE){
+  url <- "https://www.dadostransparentes.com.br/"
+  
+  obitos <- read_html(url) %>% 
+    html_nodes('iframe') %>% 
+    extract(10) %>%
+    html_attr("src") %>% 
+    paste0(url, .) %>% 
+    read_html %>%
+    html_nodes(xpath = "//*[@id=\"table\"]") %>% 
+    html_table(header = NA, dec = ".")%>% 
+    .[[1]]
+  
+  if(dec =="."){
+    obitos <- change_separator(obitos)
+    if(save == TRUE){
+      is_empty <- ifelse(length(str_detect(list.files("dados"), 'obitos')) == 0,
+                         yes = 0, no = str_detect(list.files("dados"), 'obitos'))
+      date <- ifelse(is_empty, 
+                     str_extract(list.file(dados), '\\d*\\-\\d*\\-\\d*'),
+                     0)
+      last_file <- ifelse(test = (date > 0), 
+                          yes = get_update() - max(ymd(date)), 
+                          no = get_update())
+      if(last_file > 0){
+        name <- paste0('dados/obitos', get_update(), '.csv')
+        
+        obitos %>% 
+          as_tibble() %>%
+          write_csv(., name)
+        
+        read_csv(name)
+      }else{
+        as_tibble(obitos)
+      }
+    }else{
+      as_tibble(obitos)
+    }
+  }else{
+    if(save == TRUE){
+      is_empty <- ifelse(length(str_detect(list.files("dados"), 'obitos')) == 0,
+                         yes = 0, no = str_detect(list.files("dados"), 'obitos'))
+      date <- ifelse(is_empty, 
+                     str_extract(list.file(dados), '\\d*\\-\\d*\\-\\d*'),
+                     0)
+      last_file <- ifelse(test = (date > 0), 
+                          yes = get_update() - max(ymd(date)), 
+                          no = get_update())
+      if(test > 0){
+        obitos %>% 
+          as_tibble() %>%
+          write_csv(., paste0('obitos', get_update(), '.csv'))
+        
+      }else{
+        as_tibble(obitos)
+      }
+    }else{
+      as_tibble(obitos)
+    }
+  }
+}
